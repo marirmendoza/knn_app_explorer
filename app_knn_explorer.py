a9cc71a0f96c35b566d9c91072b5529ade4d725a
Mariana Recamonde-Mendoza <mrmendoza@inf.ufrgs.br>
2026-02-25 11:54:18 -0300

    Update app_knn_explorer.py
    Prof. -> Profa.

--- app_knn_explorer.py
+++ app_knn_explorer.py
@@ -14,7 +14,7 @@ st.set_page_config(page_title="kNN Explorer", layout="wide")
 
 st.markdown("""
 <div style="background-color:#f0f2f6; padding:15px; border-radius:10px; border-left: 5px solid #004a99;">
-    <strong>Machine Learning – Prof. Mariana Recamonde Mendoza</strong><br>
+    <strong>Machine Learning – Profa. Mariana Recamonde Mendoza</strong><br>
     Institute of Informatics, Federal University of Rio Grande do Sul (UFRGS).<br>
     <em>Interactive material developed with the support of generative AI (ChatGPT + Gemini).</em>
 </div>
